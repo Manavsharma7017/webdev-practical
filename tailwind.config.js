c2a0c63d4736e9a10e04aebf58c4254e52ba6921
Manavsharma7017 <manavsharma7017@gmail.com>
2024-12-23 18:54:18 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        manav:"#257ca3",
+        chinki:"#dcf3ff",
+        aman:"#a2d2df"
+      }
+    },
+  },
+  plugins: [],
+}
